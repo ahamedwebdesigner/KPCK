b5777149d3654782b4a9fa2bc047ba396bbe291e
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2021-05-31 15:27:54 +0530

    making async task using promis

--- kpvk/javascriptBox/index.js
+++ kpvk/javascriptBox/index.js
@@ -1,4 +1,68 @@
 console.log("script started---------------->");
+/*
+// prob1: cont access async valur 
+function someAsyncFunction(name){
+    setTimeout(()=>{
+      return  name.toLocaleUpperCase()
+    },3000) ;
+}
+
+
+function consumenr(){
+    let getGetName = someAsyncFunction("Mustaq");
+    console.log(getGetName);
+}
+consumenr();
+
+////////// using promis 
+
+function someAsyncFunction(name){
+  return new Promise((res,reg)=>{
+    setTimeout(()=>{
+        res( name.toLocaleUpperCase());
+      },3000) ;
+  });
+}
+
+function consumenr(){
+    let getGetName = someAsyncFunction("Mustaq");
+
+    getGetName.then(
+        (result)=>{ console.log(result)},
+        (error)=>{console.log(error)},
+    );
+
+}
+consumenr();
+
+*/
+
+
+
+
+/*
+
+let promise = new Promise((resolve, reject)=>{
+    // async task 
+    let asynctaskStatus = false;
+
+    if(asynctaskStatus){
+        resolve("I am resolved");
+    }else{
+        reject("Error occured");
+    }
+});
+
+
+promise.then((result)=>{
+    console.log(result)
+},(error)=>{
+    console.log(error);
+});
+
+*/
+
+
 
 
 console.log("script ended------------------>");

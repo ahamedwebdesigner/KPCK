0de6c16d52a6821c5619055138aff088acca0bd3
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2021-07-24 15:05:42 +0530

    crud compleated

--- app_two/readme.md
+++ app_two/readme.md
@@ -11,3 +11,17 @@ npm install -g knex
 
 
 npm install knex bookshelf mysql
+
+
+## 5
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
--- app_two/routes/index.js
+++ app_two/routes/index.js
@@ -25,16 +25,118 @@ const knex = require('knex')({
   }});
 
 
-  const bookshelf = require('bookshelf')(knex)
+  const bookshelf = require('bookshelf')(knex);
+
+  var User = bookshelf.Model.extend({
+    tableName: 'users'
+  });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- 
-  console.log("==========================#knex======================");
-  console.log(bookshelf);
-  console.log("==========================#knex======================");
-  res.render('index', { title: 'Express' });
+ /*
+  new User()
+    .fetchAll()
+		.then(function (users) {
+			res.json(users);
+    })
+    .catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+    });
+*/
+
+    new User().where('id', 1)
+		.destroy()
+		.catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+    
+  // console.log("==========================#knex======================");
+  // console.log(bookshelf);
+  // console.log("==========================#knex======================");
+  //res.render('index', { title: 'Express' });
 });
+
+
+
+router.get('/user-create', function(req, res, next) {
+
+  new User({
+		username: "mustaq",
+		email:'mustaq@gmail.com',
+		name:"mushy",
+		age: '32',
+		location: 'Anantapur'
+  }).save()
+    .then(function (user) {
+      res.json(user);
+    }).catch(function (error) {
+      console.log(error);
+      res.send('An error occured');
+    });
+ 
+  // res.render('index', { title: 'Express' });
+});
+
+
+
+/* GET home page. */
+router.get('/user-delet', function(req, res, next) {
+
+
+     new User().where('id', 3)
+     .destroy()
+     .then((data)=>{
+      res.send('user removed ');
+     })
+     .catch(function (error) {
+       console.log(error);
+       res.send('An error occured');
+     });
+     
+
+ });
+ 
+
+/* GET home page. */
+router.get('/user-details', function(req, res, next) {
+
+
+  new User().where('id', 4)
+  .fetch()
+  .then(function (user) {
+    res.json(user);
+  }).catch(function (error) {
+    console.log(error);
+    res.send('An error occured');
+  });
+ 
+  
+
+});
+
+
+/* GET home page. */
+router.get('/user-update', function(req, res, next) {
+
+
+  new User().where('id', 4).save('username', 'John Smith',{
+    method: 'update',
+    patch: true
+}).then((model) => {
+    res.json(user);
+  }).catch(function (error) {
+    console.log(error);
+    res.send('An error occured');
+  });
+  
+
+});
+
+
+
 
 
 

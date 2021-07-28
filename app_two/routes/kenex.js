b81db0c97a8089519407b2b3460259f57a1c3bb2
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2021-07-28 16:04:05 +0530

    end of knex

--- app_two/routes/kenex.js
+++ app_two/routes/kenex.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var _ = require('underscore');
 
 
 
@@ -33,25 +33,43 @@ router.get('/', function(req, res, next) {
   // let dbres = knex.column('employeeNumber','lastName').select().from('employees');
   // let dbres =   knex.raw('select * from employees');
 
-
   // let dbres = knex.select().from('employees').orderBy('employeeNumber', 'asc');
-  //let dbres = knex.select().from('employees').orderByRaw('ISNULL(reportsTo), reportsTo ASC');
+  // let dbres = knex.select().from('employees').orderByRaw('ISNULL(reportsTo), reportsTo ASC');
  
   // let dbres = knex.column('employeeNumber','lastName').select().from(function(){
   //         this.from('employees').groupBy('jobTitle').as('t1')
   // });
 
+  // knex.avg('sum_column1').from(function() {
+  //   this.sum('column1 as sum_column1').from('t1').groupBy('column1').as('t1')
+  // }).as('ignored_alias')
+
+  // let dbres = knex('employees').whereNot({
+  //   employeeNumber: '1088',
+  //   reportsTo:  '1102'
+  // }).select('reportsTo')
 
 
-  knex.avg('sum_column1').from(function() {
-    this.sum('column1 as sum_column1').from('t1').groupBy('column1').as('t1')
-  }).as('ignored_alias')
+  // let dbres =knex('users')
+  // .join('contacts', 'users.id', '=', 'contacts.user_id')
+  // .select('users.id', 'contacts.phone') 
 
 
+  let dbres = knex('employees')
+  .join('offices', 'employees.officeCode', '=', 'offices.officeCode')
+  .whereNot({
+      employeeNumber: '1002',
+    })
+  .select('employees.employeeNumber', 'employees.lastName', 'offices.city' )
+
+
+  
   dbres.then(data=>{
-      res.json(data);
+    let optionData= _.object(_.pluck(data,'employeeNumber'),_.pluck(data,'lastName')) ;
+
+      res.json(optionData);
   })
-  //  res.json(JSON.stringify(dbres));
+  // res.json(JSON.stringify(dbres));
 
   console.log("==========================#knex======================");
   console.log(dbres.toSQL());

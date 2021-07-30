6dbad5ca157fe89843af01bce824d5a70ec64c53
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2021-07-30 18:25:58 +0530

    created books and authors

--- app_two/data/migrations/20210730124245_create_authors_table.js
+++ app_two/data/migrations/20210730124245_create_authors_table.js
@@ -2,15 +2,14 @@ exports.up = function(knex) {
     return knex.schema
     .dropTableIfExists('authors')
     .createTable('authors', function(t) {
-        t.increments('authorId').primary();
+        t.increments().primary()
    
         t.string('authorName', 255).notNullable()
         t.string('email', 255).notNullable()
         t.string('password', 255).notNullable()
         t.boolean('account_verified').notNullable().defaultTo(false)
-        
-         t.timestamp('created_at').defaultTo(knex.fn.now())
-         t.timestamp('updated_at').defaultTo(knex.fn.now())
+        t.timestamp('created_at').defaultTo(knex.fn.now())
+        t.timestamp('updated_at').defaultTo(knex.fn.now())
 
     });
 };
--- app_two/data/migrations/20210730124255_create_books_table.js
+++ app_two/data/migrations/20210730124255_create_books_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('books', function(t) {
+        t.increments().primary()
+        t.string('title', 255).notNullable()
+        t.string('description', 255).notNullable(),
+        t.integer('authorId',11).unsigned().notNullable();  
+        t.foreign('authorId').references('id').inTable('authors');
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('books');
 };

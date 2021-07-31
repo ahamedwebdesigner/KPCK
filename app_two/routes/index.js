9b772f845396d8f75f2b8fe9ff75c1d91878b948
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2021-07-31 17:16:14 +0530

    adding konex event to log query excuited

--- app_two/routes/index.js
+++ app_two/routes/index.js
@@ -10,16 +10,46 @@ const knex = require('knex')({
       password: '123456789',
       database: 'apptwo'
   }});
+ const bookshelf = require('bookshelf')(knex);
+
+ knex.on( 'query', function( queryData ) {
+  console.log( "--------------Query data--------------------" );
+  console.log( queryData.sql);
+  console.log( "----------------------------------------" );
+});
 
 
-  const bookshelf = require('bookshelf')(knex);
+module.exports = bookshelf.model('Author', {
+  tableName: 'authors',
+  book() {
+    return this.hasMany('Book')
+  }
+});
+
+module.exports = bookshelf.model('Book', {
+  tableName: 'books',
+  author() {
+      return this.belongsTo("Author");
+  }
+});
+const Author = bookshelf.model('Author')
+const Book = bookshelf.model('Book')
 
 
   
 /* GET home page. */
 router.get('/', async function(req, res, next) {
  
- 
+
+
+
+  try {
+    let  allGere = await Book.count();
+    // allGere.forEach(e=>console.log(e.toJSON()))
+    console.log(allGere);
+  } catch (error) {
+    
+  }
     
   
   res.render('index', { title: 'Express' });
